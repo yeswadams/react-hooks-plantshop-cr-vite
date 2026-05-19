97993e7285b4978793fb8a1edf18b4d99bc1d2ac
yeswadams <yeswaadams73@gmail.com>
2026-05-19 20:57:29 +0300

    implemented the search feature

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const displayedPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
